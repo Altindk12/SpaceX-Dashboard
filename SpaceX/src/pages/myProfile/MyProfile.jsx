2034d8d1502d20b9942f5acbf6388d6b05890a56
Altin Daka <altindaka1@gmail.com>
2024-02-08 00:33:14 +0100

    improved MyProfile UI

--- SpaceX/src/pages/myProfile/MyProfile.jsx
+++ SpaceX/src/pages/myProfile/MyProfile.jsx
@@ -8,36 +8,43 @@ const MyProfile = () => {
   const joinedMissions = missions.filter((mission) => mission.reserved);
 
   return (
-    <div className="my-profile">
-      <div className="stats">
-        <div className="item-stats grid-item">
-          <h2>Reserved Rockets</h2>
-          <div className="rocket-list">
-            {reservedRockets.length !== 0 ? (
-              reservedRockets.map((rocket) => (
+    <div className="my-profile-container">
+      <div className="section">
+        <h2 className="section-title">Reserved Rockets</h2>
+        <div className="content">
+          {reservedRockets.length !== 0 ? (
+            <div className="rocket-list">
+              {reservedRockets.map((rocket) => (
                 <div key={rocket.id} className="rocket">
-                  <p>{rocket.rocket_name}</p>
+                  <p className="rocket-name">{rocket.rocket_name}</p>
                   <div className="rocket-animation">🛰️</div>
                 </div>
-              ))
-            ) : (
-              <p>No rockets reserved!</p>
-            )}
-          </div>
+              ))}
+            </div>
+          ) : (
+            <p className="no-content-message">
+              No rockets reserved!{" "}
+              {"            🡠Reserve  a rocket if you want to appear here "}
+            </p>
+          )}
         </div>
-        <div className="item-stats grid-item">
-          <h2>Joined Missions</h2>
-          <div className="mission-list">
-            {joinedMissions.length !== 0 ? (
-              joinedMissions.map((mission) => (
+      </div>
+      <div className="section">
+        <h2 className="section-title">Joined Missions</h2>
+        <div className="content">
+          {joinedMissions.length !== 0 ? (
+            <div className="mission-list">
+              {joinedMissions.map((mission) => (
                 <div key={mission.mission_id} className="mission">
-                  <p>{mission.mission_name}</p>
+                  <p className="mission-name">{mission.mission_name}</p>
                 </div>
-              ))
-            ) : (
-              <p>No missions joined!</p>
-            )}
-          </div>
+              ))}
+            </div>
+          ) : (
+            <p className="no-content-message">
+              No missions joined!{"🡠Join a mission  if you want to appear here"}
+            </p>
+          )}
         </div>
       </div>
     </div>
